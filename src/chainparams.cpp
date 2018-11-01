cf2f05894ddef65dd7b0b4ee18618d79eeb53eb3
copicogithub1 <copicocareers@gmail.com>
2018-11-01 10:57:31 +0000

    set checkpoints for fork

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -78,7 +78,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-	1532896389, // * UNIX timestamp of last checkpoint block
+	1541066020, // * UNIX timestamp of last checkpoint block
     0,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
